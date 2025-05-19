8cbbe21eb64b9b7e076daf5757e4a5f7aadeff04
IceTea4 <aistis.jakutonislearn@gmail.com>
2025-05-19 23:49:09 +0300

    Add files via upload

---
+++ lab2/lab2-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    private static int actionCount = 0;
+
+    static void Main()
+    {
+        int n = 1000;
+        long result = Method(n);
+
+        Console.WriteLine(actionCount);
+    }
+
+    public static long Method(int n)
+    {
+        if (n <= 1)
+        {
+            return 1;
+        }
+
+        for (int i = 0; i < (n - 1); i++)
+        {
+            actionCount++;
+        }
+
+        Method(n / 9);
+        Method(n / 9);
+
+        return actionCount;
+    }
+}
+
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 16000;
+        int[] testArray = new int[n];
+        Console.WriteLine(testArray.Length);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        long result = methodToAnalysis(testArray);
+
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+
+    public static long methodToAnalysis(int[] arr)
+    {
+        long n = arr.Length;
+        long k = n;
+
+        for (int i = 0; i < n * 2; i++)
+        {
+            for (int j = 0; j < n / 2; j++)
+            {
+                k -= 2;
+            }
+        }
+
+        return k;
+    }
+}
